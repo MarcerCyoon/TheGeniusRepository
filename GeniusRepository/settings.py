58d88292bfd9fbbfff59a70ba6944fd484952333
MarcerCyoon <4866635+MarcerCyoon@users.noreply.github.com>
2024-09-11 02:35:04 -0400

    reverted back to the other static folder

--- GeniusRepository/settings.py
+++ GeniusRepository/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/GeniusRepository/staticfiles/'
+STATIC_URL = '/static'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # import-export safety settings
